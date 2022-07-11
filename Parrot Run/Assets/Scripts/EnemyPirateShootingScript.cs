7cf3883bea25dc4a6fc3b541721d273e46b0daae
KingEsel1 <reutermarkus1@gmail.com>
2022-07-11 13:43:08 +0200

    paar kleine anpassungen an schiessendem gegner

--- Parrot Run/Assets/Scripts/EnemyPirateShootingScript.cs
+++ Parrot Run/Assets/Scripts/EnemyPirateShootingScript.cs
@@ -9,31 +9,10 @@ public class EnemyPirateShootingScript : MonoBehaviour
 
     int index;
 
-    void Awake()
-    {
-        InitObstacles();
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(EnemyPirateShoot());
-    }
-
-    void InitObstacles()
-    {
-        // initialize obstacles -> the for loop must be greater than all objects that can appear on the screen at the same time, otherwise Unity will collapse because of infinite loop
-        // -> Thats is why i < 50 * obstacles.length
-        for (int i = 0; i < 50 * enemyBullet.Length; i++)
-        {
-            listOfEnemyBullets.Add(enemyBullet[index]);
-            index++;
-
-            if (index == enemyBullet.Length)
-            {
-                index = 0;
-            }
-        }
     }
 
     IEnumerator EnemyPirateShoot()
@@ -47,33 +26,5 @@ public class EnemyPirateShootingScript : MonoBehaviour
 
         // to always spawn new objects, execute the coroutine again
         StartCoroutine(EnemyPirateShoot());
-    }
-
-    IEnumerator EnemyPirateShoot2()
-    {
-        // wait for some time till next obstacle is spawned
-        yield return new WaitForSeconds(Random.Range(2f, 5f));
-
-        // activate obstacle
-        int index = Random.Range(0, listOfEnemyBullets.Count);
-
-        while (true)
-        {
-
-            if (listOfEnemyBullets[index] != null)
-            {
-                GameObject obj = Instantiate(listOfEnemyBullets[index], transform.position, Quaternion.identity);
-                obj.SetActive(true);
-                Debug.Log("Enemy has shot");
-                break;
-            }
-            else
-            {
-                index = Random.Range(0, listOfEnemyBullets.Count);
-            }
-        }
-
-        // to always spawn new objects, execute the coroutine again
-        StartCoroutine(EnemyPirateShoot2());
     }
 }
